d21a80562711e072b9f169d66b4d8079204cd3b5
Ianitsa <iani_georgieva@mail.bg>
2016-02-26 22:15:08 +0200

    CSharp homework 6

---
+++ Streams and Files-hw6/04.Problem 4. Copy Binary File/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _04.Problem_4.Copy_Binary_File
+{
+    class Program
+    {
+        const string NImagePath = "../../image.jpg";
+        const string DestinationPath = "../../result.jpg";
+
+        static void Main(string[] args)
+        {
+            using (var source = new FileStream(NImagePath, FileMode.Open))
+            {
+                using (var destination = new FileStream(DestinationPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[100000];
+                    while (true)
+                    {
+                        int readBytes = source.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+
+                        destination.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+
+            // open image file with results
+            System.Diagnostics.Process.Start(@"..\..\result.jpg");
+        }
+        public static Microsoft.Win32.SafeHandles.SafeFileHandle ImagePath { get; set; }
+    }
+}
---
+++ Streams and Files-hw6/Problem 2. Line Numbers/LineNum.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem_2.Line_Numbers
+{
+    class LineNum
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+            StreamWriter writer = null;
+
+            // open input (current) file
+            try
+            {
+                reader = new StreamReader(@"..\..\LineNumbers.cs", Encoding.GetEncoding("windows-1251"));
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+
+            // create output file
+            try
+            {
+                writer = new StreamWriter(@"..\..\LineNumbers.txt");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to create output file.");
+            }
+
+
+            string s;
+            int lineNumber = 1;
+            using (reader)
+            using (writer)
+            {
+                do // algorithm: read one line from input file, write line number + that line to output file
+                {
+                    s = reader.ReadLine();
+                    writer.WriteLine("{0} {1}", lineNumber, s);
+                    lineNumber++;
+                } while (s != null);
+            }
+            Console.WriteLine("Task complete:\n");
+
+            // print output file to the console
+            string fileContents = File.ReadAllText(@"..\..\LineNumbers.txt");
+            Console.WriteLine(fileContents);
+    
+        }
+    }
+}
